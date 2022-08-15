77d47f150ad6904509e871ff73d13c9606b20b6b
Bence Fulop <bence.fulop@arq.group>
2022-08-15 18:48:11 +1000

    create incidentsViewController

---
+++ motorola/IncidentsViewController.swift
@@ -0,0 +1,14 @@
+//
+//  IncidentsViewController.swift
+//  motorola
+
+import UIKit
+
+class IncidentsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("🎉 here I am once again") // Do any additional setup after loading the view.
+    }
+}
