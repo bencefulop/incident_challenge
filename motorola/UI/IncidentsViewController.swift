13a5cf0fa8e48db2f2873b6e85fa0bb6d4d2166d
Bence Fulop <bence.fulop@arq.group>
2022-08-15 19:33:03 +1000

    add tableview & organise project

---
+++ motorola/UI/IncidentsViewController.swift
@@ -0,0 +1,48 @@
+//
+//  IncidentsViewController.swift
+//  motorola
+
+import UIKit
+
+class IncidentsViewController: UIViewController {
+
+    
+
+    var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureTableView()
+    }
+    
+    
+    private func configureTableView() {
+        tableView = UITableView(frame: view.bounds, style: .plain)
+        view.addSubview(tableView)
+        tableView.backgroundColor = .systemBackground
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+   
+}
+
+// - MARK: UITableViewDataSource
+extension IncidentsViewController: UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+
+}
+
+// - MARK: UITableViewDataSource
+extension IncidentsViewController: UITableViewDelegate { }
+
+
+
+
