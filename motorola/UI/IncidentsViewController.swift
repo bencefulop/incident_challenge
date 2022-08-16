02575e0040e727d0c7669e1c46d12fff4990ca4e
Bence Fulop <bence.fulop@arq.group>
2022-08-16 20:21:38 +1000

    load test data on tableview

--- motorola/UI/IncidentsViewController.swift
+++ motorola/UI/IncidentsViewController.swift
@@ -7,21 +7,27 @@ import UIKit
 class IncidentsViewController: UIViewController {
 
     var tableView: UITableView!
+    let dataManager = DataManager.shared
+    var incidents: [Incident] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         configureTableView()
-        DataManager.shared.getSortedIncidents()
-        let values = DataManager.shared.incidents
-        print("🎉 \(values)")
+        loadIncidents()
     }
 
     private func configureTableView() {
         tableView = UITableView(frame: view.bounds, style: .plain)
         view.addSubview(tableView)
         tableView.backgroundColor = .systemBackground
+        tableView.rowHeight = 60
         tableView.delegate = self
         tableView.dataSource = self
+    }
+    
+    private func loadIncidents() {
+        dataManager.getSortedIncidents()
+        incidents = dataManager.incidents
     }
    
 }
@@ -31,13 +37,14 @@ extension IncidentsViewController: UITableViewDataSource {
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
+        cell.textLabel?.text = "tests"
         return cell
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 4
+        return incidents.count
     }
-
+    
 }
 
 // - MARK: UITableViewDataSource
